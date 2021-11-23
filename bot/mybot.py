3092b8f366ec3697212447bbea910d8110b57bcb
Vlad <blackdp@ya.ru>
2021-11-23 16:39:24 +0300

    Убрал ключи

--- bot/mybot.py
+++ bot/mybot.py
@@ -1,19 +1,31 @@
-from telegram.ext import Updater, CommandHandler
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+import settings
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 def greet_user(update, context):
     print('Вызван /start')
     print (update)
     update.message.reply_text('Привет, ты начал общаться с ботом, вызвав /start')
 
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+
+
 
 def main():
-    mybot = Updater('2036491217:AAEfksApd3IcVvmjxPscj_d7cMp3OomEyjM', use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
-
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    logging.info('Бот стартовал')
     mybot.start_polling()
     mybot.idle()
 
-main()
+if __name__ == '__main__':
+    main()
